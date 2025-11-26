fc06e3936c523702b4ce95dd46841b7d6eeceba8
jacky <jackyfan@163.com>
2025-11-26 13:57:44 +0800

    vector store

---
+++ vector-store-loader/src/test/java/com/jackyfan/handsonspringai/vectorstoreloader/VectorStoreLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackyfan.handsonspringai.vectorstoreloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorStoreLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
