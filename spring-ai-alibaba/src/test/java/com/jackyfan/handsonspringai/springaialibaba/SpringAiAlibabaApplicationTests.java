a95c1206e6e94772423152aa027eb9f7cbcb8cc6
jacky <jackyfan@163.com>
2025-12-10 18:11:55 +0800

    init spring ai alibaba

---
+++ spring-ai-alibaba/src/test/java/com/jackyfan/handsonspringai/springaialibaba/SpringAiAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackyfan.handsonspringai.springaialibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAlibabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
